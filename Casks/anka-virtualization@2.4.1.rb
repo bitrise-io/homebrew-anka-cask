51a6fb138d4cbbbfb8b66b257d7112e7c139a53d
bitrise-tom <tom.ashley@bitrise.io>
2021-06-14 16:21:28 +0200

    Veertu moved to vanity URL for packages

--- Casks/anka-virtualization@2.4.1.rb
+++ Casks/anka-virtualization@2.4.1.rb
@@ -2,8 +2,8 @@ cask "anka-virtualization@2.4.1" do
   version "2.4.1.130"
   sha256 "30917cc046ea6cd1de135c11fd1e0151e521c4974916fab6ee9f9c82ff8ba21b"
 
-  url "https://d1efqjhnhbvc57.cloudfront.net/Anka-#{version}.pkg",
-      verified: "d1efqjhnhbvc57.cloudfront.net/"
+  url "https://downloads.veertu.com/anka/Anka-#{version}.pkg",
+      verified: "downloads.veertu.com/anka/"
   appcast "https://ankadocs.veertu.com/docs/release-notes/"
   name "Anka Virtualization"
   desc "CLI tool for managing and creating virtual machines"
