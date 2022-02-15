bac1f45ed3c1b5cdf06c2c58ef397d02469a6bf8
Sandor Feher <43136024+fehersanyi-bitrise@users.noreply.github.com>
2022-02-15 10:44:33 +0100

    [IN-3349] Add cask for latest Anka version (#13)

---
+++ Casks/anka-virtualization@3.0.0.140.rb
@@ -0,0 +1,45 @@
+cask "anka-virtualization@3.0.0.140" do
+  version "3.0.0.140"
+  sha256 "f6d981a5939450f593b3d720513d9bd8ac4c4fdd3ed866b85912c942663af9d5"
+
+  url "https://downloads.veertu.com/anka/Anka-#{version}.pkg",
+      verified: "downloads.veertu.com/anka/"
+  appcast "https://ankadocs.veertu.com/docs/release-notes/"
+  name "Anka Virtualization"
+  desc "CLI tool for managing and creating virtual machines"
+  homepage "https://veertu.com/"
+
+  depends_on macos: ">= :yosemite"
+
+  pkg "Anka-#{version}.pkg"
+
+  uninstall launchctl: [
+    "com.veertu.nlimit",
+    "com.veertu.vlaunch",
+    "com.veertu.anka.lupd",
+    "com.veertu.anka.ankakbd",
+    "com.veertu.anka.ankanetd",
+  ],
+            script:    {
+              executable: "/Library/Application Support/Veertu/Anka/tools/uninstall.sh",
+              args:       ["-f"],
+              sudo:       true,
+            }
+
+  zap trash: [
+    "~/.anka",
+    "~/Library/Application Support/Veertu/Anka",
+    "~/Library/Application Support/CrashReporter/ankahv_*.plist",
+    "~/Library/Logs/Anka",
+    "~/Library/Preferences/com.veertu.ankaview.plist",
+    "/Library/Application Support/Veertu/Anka",
+  ],
+      rmdir: [
+        "~/Library/Application Support/Veertu",
+        "/Library/Application Support/Veertu",
+      ]
+
+  caveats do
+    license "https://veertu.com/terms-and-conditions/"
+  end
+end
